8a1493056f90e2701509bcd338189943ccb33753
Law44 <tlaw4444@gmail.com>
2026-05-18 15:34:48 +0200

    Doesnt work

--- src/main/java/ValidadorPassword.java
+++ src/main/java/ValidadorPassword.java
@@ -13,6 +13,7 @@ public class ValidadorPassword {
     }
 
     private boolean tieneAlMenosUnDigito(String s) {
+        System.out.println("Esto va a dar error");
         for (char c : s.toCharArray()) {
             if (Character.isDigit(c)) return true;
         }
